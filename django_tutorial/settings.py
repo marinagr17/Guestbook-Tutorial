f0ab2572bfeeeb3e0ac7553d3b2d70378a979073
Marina <mgracia1411@gmail.com>
2025-12-10 08:54:30 +0000

    Proyecto Django subida inicial

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9f0h)gozf$g%6igo8&767w1xro0adm+)msxe)!eic$!fhvynb8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,5 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
+
+STATIC_ROOT = BASE_DIR / "static"
